471bf668d1b24c47167eccf198518a7b5208d10b
Thallison Rodrigues <thallisonsiqueira@gmail.com>
2023-05-18 16:45:13 -0300

    itensmenu

--- myScript.js
+++ myScript.js
@@ -1,6 +1,7 @@
 function clickMenu(){
     if(itensMenu.style.display == 'block'){
         itensMenu.style.display = 'none';
+        itensMenu.a.style.display = 'none';
     }else{
         itensMenu.style.display = 'block';
     }  
